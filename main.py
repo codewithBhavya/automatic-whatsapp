1f55a2a76f675b8f2580a86d7f06bd887bd0ba5f
Bhavya <76579579+codewithBhavya@users.noreply.github.com>
2021-04-04 17:17:42 +0530

    whatsapp
    this file will send a whatsapp message through web whatsapp

---
+++ main.py
@@ -0,0 +1,5 @@
+"""123456 is a sample number enter the number you want to send your message and time in 24 hours time"""
+import pywhatkit
+pywhatkit.sendwhatmsg(
+    "123456", 'hello world', 12, 00
+)
